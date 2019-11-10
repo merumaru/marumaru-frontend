12e58ed4bee678c98d8c54f7db56ae5b8879697a
kaustubhhiware <hiwarekaustubh@gmail.com>
2019-11-10 13:42:49 +0900

    Added login and signup pages

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -1,6 +1,33 @@
 export default function() {
   return [
     {
+      title: "Home", 
+      to: "/home",
+      htmlBefore: '<i class="material-icons">explore</i>',
+      htmlAfter: ""
+    },
+    {
+      title: "Product Details", 
+      to: "/products/1",
+      htmlBefore: '<i class="material-icons">description</i>',
+      htmlAfter: ""
+    },
+    {
+      title: "Add New Product",
+      htmlBefore: '<i class="material-icons">note_add</i>',
+      to: "/add-product",
+    },
+    {
+      title: "User Profile",
+      htmlBefore: '<i class="material-icons">person</i>',
+      to: "/user/:id",
+    },
+    {
+      title: ".",
+      htmlBefore: '<i class="material-icons">code</i>',
+      to: "/errors",
+    },
+    {
       title: "Blog Dashboard",
       to: "/blog-overview",
       htmlBefore: '<i class="material-icons">edit</i>',
@@ -25,11 +52,6 @@ export default function() {
       title: "Tables",
       htmlBefore: '<i class="material-icons">table_chart</i>',
       to: "/tables",
-    },
-    {
-      title: "User Profile",
-      htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
     },
     {
       title: "Errors",
